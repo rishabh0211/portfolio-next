228577a5a0240bc64cbab34c1733e7d1190cf1c9
Rishabh Rastogi <rishabh0211@gmail.com>
2024-10-14 16:58:04 +0530

    experience section head

--- src/components/sections/Experience.tsx
+++ src/components/sections/Experience.tsx
@@ -1,14 +1,29 @@
+'use client';
 import Heading from '../ui/heading';
 
-const JOBS = ['', 'Neighborhoods', 'AvataraCloud', 'Caroobi', 'Wisitech', 'Accenture'];
+const JOBS = [
+	'BåerKarrer',
+	'Law Catch',
+	'Rosy Labs',
+	'ContractpodAI',
+	'Neighborhoods',
+	'AvataraCloud',
+	'Caroobi',
+	'Wisitech',
+	'Accenture',
+];
 
 const Experience = () => {
 	return (
 		<section id='jobs' className='pt-20 pb-24 px-5 bg-background_light'>
 			<Heading heading='Professional Experience' />
 			<div className='mt-4'>
-				<ul>
-					<li></li>
+				<ul className='w-full flex overflow-x-scroll border-b-2 border-foreground'>
+					{JOBS.map((job) => (
+						<p key={job} className='text-nowrap py-2 px-4'>
+							{job}
+						</p>
+					))}
 				</ul>
 			</div>
 		</section>
